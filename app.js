39164555970d983f4e7540d42fa701189b062172
samuelguesnier <thenemesisa5@gmail.com>
2018-01-18 01:20:54 +0100

    Coords carte + placement point sur image

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
+var point = function(){
+	this.image = new Image();
+	this.image.src = 'marker.png';
+	this.hauteur = 20;
+	this.largeur = 20;
+	this.coordX = 0;
+	this.coordY = 0;
+	this.titre = "";
+	this.description = "";
+};
+var points = new Array();
 
-var canvas = document.getElementById("#img3");
-/**function afficherCoords(evenement,img){
-        console.log(
-          "clientX value: " + evenement.clientX + "\n" +
-          "clientY value: " + evenement.clientY + "\n"
-        );
-      }**/
+var c1 = document.getElementById("Canvas");
+function imagePrincipale() {
+    var c = document.getElementById("Canvas");
+    var ctx = c.getContext("2d");
+    var img = new Image();   // Crée un nouvel élément Image
+    var marker = new Image();
+    img.onload = function() {
+    	c.width = img.width;
+    	c.height = img.height;
+    	ctx.drawImage(img,0,0);
+  };
+  
+	img.src = 'image.jpeg'; 
+	marker.src = 'marker.png';
+   
+	c.addEventListener('click',function(event){
+    	var rect = c.getBoundingClientRect();
+    	var x = event.clientX - rect.left;
+    	var y = event.clientY - rect.top;
+    	ctx.drawImage(marker,x-10,y-10,20,20);
+    	console.log("x: " + x + " y: " + y);
 
-
-
-function afficherCoords(canvas, event) {
-    var rect = canvas.getBoundingClientRect();
-    var x = event.clientX - rect.left;
-    var y = event.clientY - rect.top;
-    console.log("x: " + x + " y: " + y);
+	},false);
 }
 
+function enregisterMarkers(event){
+	marker = new point();
+}
 
-//afficherCoords(canvas,e);
+function placerMarkers(argument) {
+	// body...
+}
